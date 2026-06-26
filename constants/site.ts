884a3490925c2da42129e370e8424c08bbcf3f47
Harsh Goyal <harshgoyal8nov2001@gmail.com>
2026-06-26 18:30:06 +0530

    feat: configure global theme and project foundation

---
+++ constants/site.ts
@@ -0,0 +1,11 @@
+export const SITE = {
+  name: "GS Associate",
+
+  description: "Premium Plywood & Interior Solutions",
+
+  phone: "+91-XXXXXXXXXX",
+
+  email: "info@gsassociate.com",
+
+  location: "Madhya Pradesh, India",
+};
